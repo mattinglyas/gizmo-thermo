996e0b84743078841af03c5556ebd4926db35c85
Alexander Mattingly <mattinglyas@stny.rr.com>
2020-11-06 21:37:50 -0500

    initial commit

---
+++ lib/Config/src/Config.h
@@ -0,0 +1,3 @@
+#define CONFIG_CONNECTION_STRING "HostName=amatti-iot-hub.azure-devices.net;DeviceId=MyNodeDevice;SharedAccessKey=/04mGd5Nk5sQWjqD+s0+dkLmptmUfH0XtFTo2BEgZbs="
+#define CONFIG_WIFI_NAME "Make Wifi Great Again"
+#define CONFIG_WIFI_PASSWORD "Osage42069"
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Wifi.h>
+#include "ArduinoJson.h"
+#include "FreeRTOS.h"
+#include "AzureIotHub.h"
+#include "ESP32MQTTClient.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
