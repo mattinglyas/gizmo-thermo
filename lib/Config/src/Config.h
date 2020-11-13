e0e6bf7c93f098188796b3f80b8e18ed43286fb0
Alexander Mattingly <mattinglyas@stny.rr.com>
2020-11-13 16:42:44 -0500

    implement PID control, add JSON message parsing

--- lib/Config/src/Config.h
+++ lib/Config/src/Config.h
@@ -1,3 +1,3 @@
-#define CONFIG_CONNECTION_STRING "HostName=amatti-iot-hub.azure-devices.net;DeviceId=MyNodeDevice;SharedAccessKey=/04mGd5Nk5sQWjqD+s0+dkLmptmUfH0XtFTo2BEgZbs="
+#define CONFIG_CONNECTION_STRING "HostName=onlinelabs-iot.azure-devices.net;DeviceId=gizmo6;SharedAccessKey=W2SPDXOsBihrkUXPrSll+sGkVvnrzwbku+6YcN6MNRo="
 #define CONFIG_WIFI_NAME "Make Wifi Great Again"
 #define CONFIG_WIFI_PASSWORD "Osage42069"
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 #include "ESP32MQTTClient.h"
 #include "Config.h"
 #include "PID_v1.h"
+#include "analogWrite.h"
 
-#define MESSAGE_MAX_LEN 16384 // size of message buffers
+#define MESSAGE_MAX_LEN 4096 // size of message buffers
 
 #define WIFI_TIMEOUT_MS 10000 // timeout for connecting to wifi network before re-trying
 
@@ -15,9 +16,9 @@
  * Pin definitions
  * *******************************************************************************/
 
-#define ONBOARD_LED_PIN 2
-#define THERMISTOR_IN_PIN 32
-#define PWM_OUT_PIN 4
+#define PIN_ONBOARD_LED 2
+#define PIN_THERMISTOR_IN 32
+#define PIN_PWM_OUT 4
 
 /* *******************************************************************************
  * Typedefs
@@ -36,16 +37,18 @@ static const char *connectionString = CONFIG_CONNECTION_STRING;
 
 // telemetry messages
 const char *messageData = "{\"messageId\": %UL, \"temperature\": %D, \"dutyCycle\": %UD}"; // only sending telemetry messages as constant
-const char *responseMessageData = "{\"status\": %UD, \"payload\": %s}";                    // respond with a status code and payload
 
 // default configuration parameters
 static unsigned long messageCount = 1;
-static unsigned long interval = 2000; // ms between telemetry messages
-static bool messageSending = true;    // continuously send telemetry messages
-static uint64_t lastMessage;          // storage for the last message sent (calls millis())
-static bool ledValue = false;         // internal storage for value of onboard led
-static int setpoint = 0;              // target setpoint for the heater
-double kP = 20, kI = 0.6, kD = 0;     // initial parameters (best combination is kP = 20, kI = 0.4, kD = 0.5)
+static unsigned long interval = 2000;    // ms between telemetry messages
+static bool messageSending = true;       // continuously send telemetry messages
+static uint64_t lastMessage;             // storage for the last message sent (calls millis())
+static bool ledValue = false;            // internal storage for value of onboard led
+static double setpoint = 30;             // target setpoint for the heater
+static double kP = 20, kI = 0.6, kD = 0; // initial parameters (best combination is kP = 20, kI = 0.4, kD = 0.5)
+static double input, output;             // storage for measured and calculated heater values
+
+PID pid(&input, &output, &setpoint, kP, kI, kD, DIRECT);
 
 // thread safety
 SemaphoreHandle_t pidUpdateMutex;
@@ -81,6 +84,19 @@ static bool initWifi(int timeoutInMs)
   Serial.print(F("INFO: WiFi connected. Local IP Address: "));
   Serial.println(WiFi.localIP());
   return true;
+}
+
+static double readTemperature()
+{
+  // NOTE that these values are simply the scaled values (due to 4x resolution ADC) from PID_Basic_Thermistor_Calibration.ino
+  double reading;
+
+  reading = 4096 - 2 * (double)analogRead(PIN_THERMISTOR_IN);
+  reading = reading + 228; // heuristic offset so that input always > 0
+  reading = reading * 900;
+  reading = reading / 4096;
+
+  return reading;
 }
 
 /* *******************************************************************************
@@ -121,11 +137,13 @@ static int deviceMethodCallback(const char *methodName, const unsigned char *pay
   Serial.print(F("INFO: Received direct method for: "));
   Serial.println(methodName);
 
-  char *responseMessage = (char *)malloc(MESSAGE_MAX_LEN);
+  StaticJsonDocument<MESSAGE_MAX_LEN> responseDoc;
+  JsonObject responsePayload = responseDoc.createNestedObject("payload");
+
   int result = 200;
 
   StaticJsonDocument<MESSAGE_MAX_LEN> doc;
-  deserializeJson(doc, payload, MESSAGE_MAX_LEN);
+  deserializeJson(doc, payload);
   Serial.print(F("INFO: Received payload: "));
   serializeJson(doc, Serial);
   Serial.println();
@@ -133,23 +151,27 @@ static int deviceMethodCallback(const char *methodName, const unsigned char *pay
   if (strcmp(methodName, "start") == 0)
   {
     messageSending = true;
+
     Serial.println(F("INFO: Started sending telemetry messages"));
   }
   else if (strcmp(methodName, "stop") == 0)
   {
     messageSending = false;
+
     Serial.println(F("INFO: Stopped sending telemetry messages"));
   }
   else if (strcmp(methodName, "led") == 0)
   {
     ledValue = !ledValue;
-    digitalWrite(ONBOARD_LED_PIN, ledValue);
+    digitalWrite(PIN_ONBOARD_LED, ledValue);
+
     Serial.println(F("INFO: Toggled on-board LED"));
   }
   else if (strcmp(methodName, "interval") == 0)
   {
     JsonVariant newInterval = doc["interval"];
     interval = newInterval.as<int>();
+
     Serial.print(F("INFO: Changed telemetry interval to "));
     Serial.print(interval);
     Serial.println(F("ms"));
@@ -157,31 +179,66 @@ static int deviceMethodCallback(const char *methodName, const unsigned char *pay
   else if (strcmp(methodName, "setSetpoint") == 0)
   {
     JsonVariant newSetpoint = doc["setpoint"];
-    setpoint = newSetpoint.as<int>();
+    setpoint = newSetpoint.as<double>();
+
     Serial.print(F("INFO: Changed telemetry interval to "));
     Serial.print(interval);
     Serial.println(F("ms"));
   }
   else if (strcmp(methodName, "getSetpoint") == 0)
   {
-    
+    responsePayload["setpoint"] = setpoint;
+
+    Serial.println(F("INFO: Responding with setpoint value"));
   }
   else if (strcmp(methodName, "setPID") == 0)
   {
-    JsonVariant newP = doc["p"];
-    JsonVariant newI = doc["i"];
-    JsonVariant newD = doc["d"];
+    JsonVariant newP = doc["kP"];
+    JsonVariant newI = doc["kI"];
+    JsonVariant newD = doc["kD"];
 
+    // take mutex to prevent pid compute operations during value updating
+    xSemaphoreTake(pidUpdateMutex, portMAX_DELAY);
 
+    pid.SetTunings(newP.as<double>(), newI.as<double>(), newD.as<double>()); 
+
+    xSemaphoreGive(pidUpdateMutex);
+
+    Serial.print(F("INFO: Updating PID parameters to kP = "));
+    Serial.print(newP.as<double>());
+    Serial.print(F(", kI = "));
+    Serial.print(newI.as<double>());
+    Serial.print(F(", kD = "));
+    Serial.println(newD.as<double>());
   }
   else if (strcmp(methodName, "getPID") == 0)
   {
+    responsePayload["kP"] = kP;
+    responsePayload["kI"] = kI;
+    responsePayload["kD"] = kD;
 
+    Serial.println(F("INFO: Responding with PID k values"));
   }
   else if (strcmp(methodName, "getHistoricalData") == 0)
   {
-    
+    // TODO
   }
+  else 
+  {
+    // invalid direct method
+    result = 404;
+    Serial.println(F("WARNING: Received invalid command"));
+  }
+
+  responseDoc["result"] = result;
+
+  char * serializedResponse = (char*)malloc(MESSAGE_MAX_LEN);
+  serializeJson(responseDoc, serializedResponse, MESSAGE_MAX_LEN);
+
+  (*response) = (unsigned char *)serializedResponse; // should not affect anything IF we only use chars <128
+  (*responseSize) = strlen((const char *)serializedResponse);
+
+  return result;
 }
 
 /* *******************************************************************************
@@ -210,7 +267,8 @@ static void commsTask(void *pvParameters)
 
   lastMessage = millis();
 
-  while (true)
+  // forever
+  for(;;)
   {
     /*  1. If I have wifi, check if it is time to send telemetry messages
      *  2. If it is time, send a message to azure and update the next telemetry time
@@ -221,11 +279,20 @@ static void commsTask(void *pvParameters)
       if (messageSending && (int)(millis() - lastMessage) >= interval)
       {
         char messagePayload[MESSAGE_MAX_LEN];
+        StaticJsonDocument<MESSAGE_MAX_LEN> doc;
 
+        doc["messageId"] = messageCount;
+        doc["temperature"] = readTemperature();
+        doc["dutyCycle"] = output;
 
-        // copy into message
-        // TODO status messages
-        
+        serializeJson(doc, messagePayload);
+
+        Serial.print(F("INFO: Sending message to IoT Hub: "));
+        Serial.println(messagePayload);
+
+        EVENT_INSTANCE *message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
+        Esp32MQTTClient_SendEventInstance(message);
+
         lastMessage = millis();
       }
 
@@ -242,17 +309,68 @@ static void commsTask(void *pvParameters)
 
 static void pidTask(void *pvParameters) 
 {
+  // initialize the variables that have been linked to the PID controller
+  input = readTemperature();
 
+  // turn the PID on
+  pid.SetMode(AUTOMATIC);
+
+  // forever
+  for (;;) 
+  {
+    input = readTemperature();
+
+    // take mutex to prevent values from updating during pid compute operations
+    xSemaphoreTake(pidUpdateMutex, portMAX_DELAY);
+
+    pid.Compute();
+    analogWrite(PIN_PWM_OUT, output);
+
+    xSemaphoreGive(pidUpdateMutex);
+
+    vTaskDelay(100);
+  }
 }
 
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(ONBOARD_LED_PIN, OUTPUT);
-  digitalWrite(ONBOARD_LED_PIN, true);
+  pinMode(PIN_THERMISTOR_IN, INPUT);
+  pinMode(PIN_ONBOARD_LED, OUTPUT);
+  pinMode(PIN_PWM_OUT, OUTPUT);
+  
+  digitalWrite(PIN_ONBOARD_LED, HIGH);
+
+  // begin setup
+  Serial.begin(115200);
+  Serial.println(F("*** Gizmo 6 ***"));
+  Serial.println(F("INFO: Initializing..."));
+
+  pidUpdateMutex = xSemaphoreCreateMutex();
+
+  xTaskCreatePinnedToCore(
+      commsTask,
+      "Communications Task",
+      65536,
+      NULL,
+      1,
+      &commsTaskHandler,
+      0);
+
+  xTaskCreatePinnedToCore(
+      pidTask,
+      "PID Controller Task",
+      16384,
+      NULL,
+      1,
+      &pidTaskHandler,
+      1);
+
+  // finish setup
+  digitalWrite(PIN_ONBOARD_LED, LOW);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // do nothing in the loop task
+  vTaskSuspend(NULL);
 }
